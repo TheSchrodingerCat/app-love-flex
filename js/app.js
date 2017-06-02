99218d638a2be61805d4c6c4ae441f62f5693543
TheSchrodingerCat <mariaj.pozom@gmail.com>
2017-06-02 17:58:54 -0400

    modal listo para elementos pequeños

--- js/app.js
+++ js/app.js
@@ -1,3 +1,42 @@
-/*
- * Archivo principal de funcionalidad de JS
- */
+
+(function main(){
+
+	var modal = document.getElementById("box-work-modal");
+	var boxes = Array.from(document.getElementsByClassName("cajita"));
+
+	var text, bodyModal, close, img;
+	boxes.forEach(function(box){
+		box.addEventListener("click",function(){
+			modal.innerHTML = "";
+
+			text = document.getElementById("texto" + boxes.indexOf(box).toString());
+			text.classList.toggle("hide");
+
+			bodyModal = document.createElement("div");
+			bodyModal.classList.add("modal-body");
+			bodyModal.innerHTML = box.innerHTML;
+			modal.appendChild(bodyModal);
+
+			modal.classList.remove("hide");//?
+
+			close = document.createElement("div");
+			close.classList.add("close");
+
+			//boton close
+			img = document.createElement("img");
+			img.setAttribute("src","http://tojaeurope.com/gallery/Close-icon.png");
+
+			close.appendChild(img);
+			modal.appendChild(close);
+
+			close.addEventListener("click",function(){
+				modal.classList.add("hide");
+				text.classList.toggle("hide");
+			})
+		})
+	});
+
+	
+	
+})();
+
